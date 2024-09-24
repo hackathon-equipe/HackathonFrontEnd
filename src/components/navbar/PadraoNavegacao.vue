cfd40e16b00a66735428c544be97b99988e66946
vinyzada <vinyescolas@gmail.com>
2024-09-23 21:10:43 -0300

    feat: alterado prioridade do menu para a max #5

--- src/components/navbar/PadraoNavegacao.vue
+++ src/components/navbar/PadraoNavegacao.vue
@@ -4,49 +4,42 @@ import { ref } from 'vue';
 import logoStandart from '@/assets/logo/logoStandart.vue';
 import { shopingCartIcon, searchIcon } from '../icons';
 const titles = [
-    {
-      text: 'Home',
-      link: '/'
-    },
-    {
-      text: 'Produtos',
-      link: '/produtos',
-      hover: 'alertfunction'
-    },
-    {
-      text: 'Orcamentos',
-      link: '/orcamentos'
-    },
-    {
-      text: 'Sobre',
-      link: '/sobre'
-    }
-  ]
+  {
+    text: 'Home',
+    link: '/'
+  },
+  {
+    text: 'Produtos',
+    link: '/produtos',
+    hover: 'alertfunction'
+  },
+  {
+    text: 'Orcamentos',
+    link: '/orcamentos'
+  },
+  {
+    text: 'Sobre',
+    link: '/sobre'
+  }
+]
 const showMenu = ref(false)
 function onHover(e) {
-  if(e == 'Produtos'){
+  if (e == 'Produtos') {
     showMenu.value = true
   }
 }
 </script>
 
 <template>
-  <div class="navbar"> 
+  <div class="navbar">
     <div class="logo">
       <router-link to="/">
         <logoStandart />
       </router-link>
     </div>
     <ul class="link-list">
-      <ListaTitulos
-        v-for="(text, index) in titles"
-        :key="index"
-        :title="text.text"
-        :link="text.link"
-        :onHover="onHover"
-        :onLeave="onLeave"
-        class="link"
-      />
+      <ListaTitulos v-for="(text, index) in titles" :key="index" :title="text.text" :link="text.link" :onHover="onHover"
+        :onLeave="onLeave" class="link" />
     </ul>
     <div class="utilities">
       <span>
@@ -77,14 +70,14 @@ function onHover(e) {
           <li>Estrutura</li>
           <li>Baterias</li>
         </ul>
-      </div> 
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped>
 /* CSS SUBMENU */
-.teste{
+.teste {
   z-index: 9999999;
   position: fixed;
   width: 100vw;
@@ -94,46 +87,55 @@ function onHover(e) {
   justify-content: center;
   border-bottom: 1px solid #CFCFCF;
 }
-.submenu{
+
+.submenu {
   width: 1400px;
   padding: 20px 250px 30px;
   display: flex;
   justify-content: center;
 }
-.filter-menu{
+
+.filter-menu {
   padding: 15px 30px 0px 0px;
   border-right: 1px solid #5292D0;
 }
-.filters{
+
+.filters {
   padding: 0px 30px 20px 0px;
   gap: 15px;
   display: flex;
   flex-direction: column;
   list-style: none
 }
-.filters li{
+
+.filters li {
   cursor: pointer;
 }
-.products{
+
+.products {
   padding: 15px 0px 0px 60px;
   display: flex;
   flex-wrap: wrap;
   gap: 15px;
   list-style: none;
 }
-.products li{
+
+.products li {
   width: 20%;
   cursor: pointer;
 }
-.products li:hover{
-  text-decoration:underline;
+
+.products li:hover {
+  text-decoration: underline;
 }
 
-.active{
+.active {
   font-weight: 600;
 }
+
 /* FIM -> CSS SUBMENU */
-.navbar{
+.navbar {
+  z-index: 999999999;
   position: fixed;
   width: 100vw;
   height: 40px;
@@ -157,10 +159,12 @@ function onHover(e) {
 .navbar .link-list .link:nth-child(1) {
   font-weight: 600;
 }
-.navbar .link-list .link:nth-child(2):hover .submenu{
+
+.navbar .link-list .link:nth-child(2):hover .submenu {
   background-color: red;
 }
-.navbar .link-list .link{
+
+.navbar .link-list .link {
   text-decoration: none;
   color: black;
 }
@@ -178,7 +182,7 @@ function onHover(e) {
   cursor: pointer;
 }
 
-.navbar .utilities button{
+.navbar .utilities button {
   cursor: pointer;
   appearance: none;
   border: 1px solid #406996;
