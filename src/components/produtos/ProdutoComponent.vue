1fb6401322783199c73d202fef6a600dcd13cd52
isabellirosa <isabelli.ifc@gmail.com>
2024-09-29 10:43:41 -0300

    FIX: Arrumando propagandas e caminho do header #32

--- src/components/produtos/ProdutoComponent.vue
+++ src/components/produtos/ProdutoComponent.vue
@@ -1,51 +1,57 @@
 <script setup>
-import PadraoCarousel from '@/components/carousel/PadraoCarousel.vue'
-import TitleCarousel from '@/components/carousel/TitleCarousel.vue'
-import ImagemProduto from '@/components/produtos/imagens/ImagemProduto.vue'
-import ImagensPqn from '@/components/produtos/imagens/ImagensPqn.vue'
-import InformacoesProduto from '@/components/produtos/informacoes/InformacoesProduto.vue'
-import DescricaoProduto from '@/components/produtos/descricao/DescricaoProduto.vue'
-import ComentarioProduto from '@/components/produtos/comentarios/ComentarioProduto.vue'
-import AddComentario from '@/components/produtos/comentarios/AddComentario.vue'
-import ButtonMais from '@/components/produtos/comentarios/ButtonMais.vue'
-import FazerOrcamento from '@/components/produtos/orcamento/FazerOrcamento.vue'
-import { computed } from 'vue'
+import PadraoCarousel from "@/components/carousel/PadraoCarousel.vue";
+import TitleCarousel from "@/components/carousel/TitleCarousel.vue";
+import ImagemProduto from "@/components/produtos/imagens/ImagemProduto.vue";
+import ImagensPqn from "@/components/produtos/imagens/ImagensPqn.vue";
+import InformacoesProduto from "@/components/produtos/informacoes/InformacoesProduto.vue";
+import DescricaoProduto from "@/components/produtos/descricao/DescricaoProduto.vue";
+import ComentarioProduto from "@/components/produtos/comentarios/ComentarioProduto.vue";
+import AddComentario from "@/components/produtos/comentarios/AddComentario.vue";
+import ButtonMais from "@/components/produtos/comentarios/ButtonMais.vue";
+import FazerOrcamento from "@/components/produtos/orcamento/FazerOrcamento.vue";
+import PadraoPropagandas from "@/components/header/propagandas/PadraoPropagandas.vue";
+import PadraoCaminho from "@/components/header/caminho/PadraoCaminho.vue";
+import { computed } from "vue";
 const props = defineProps({
   img: String,
   nome: String,
   preco: String,
   parcelas: String,
-  VerMais: String
-})
+  VerMais: String,
+});
 const imgSrc = computed(() => {
-  return new URL(`../../assets/images/${props.img}.png`, import.meta.url).href
-})
+  return new URL(`../../assets/images/${props.img}.png`, import.meta.url).href;
+});
 </script>
 <template>
-  <main>
-    <div>
-      <ImagemProduto :imagem="imgSrc" />
-    </div>
-    <InformacoesProduto :nome="nome" :preco="preco" />
-  </main>
-  <ImagensPqn :imagem="imgSrc" />
+  <div>
+    <PadraoPropagandas />
+    <PadraoCaminho />
+    <main>
+      <div>
+        <ImagemProduto :imagem="imgSrc" />
+      </div>
+      <InformacoesProduto :nome="nome" :preco="preco" />
+    </main>
+    <ImagensPqn :imagem="imgSrc" />
 
-  <DescricaoProduto :VerMais="VerMais" />
-  <div class="carousel-produtos">
-    <TitleCarousel title="Produtos Similares" />
-    <PadraoCarousel />
+    <DescricaoProduto :VerMais="VerMais" />
+    <div class="carousel-produtos">
+      <TitleCarousel title="Produtos Similares" />
+      <PadraoCarousel />
+    </div>
+    <h1 class="titulo-descricao">Comentários</h1>
+    <ComentarioProduto />
+    <ComentarioProduto />
+    <ComentarioProduto />
+    <ComentarioProduto />
+    <ButtonMais />
+    <AddComentario />
+    <FazerOrcamento />
   </div>
-  <h1 class="titulo-descricao">Comentários</h1>
-  <ComentarioProduto />
-  <ComentarioProduto />
-  <ComentarioProduto />
-  <ComentarioProduto />
-  <ButtonMais />
-  <AddComentario />
-  <FazerOrcamento />
 </template>
 <style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
 
 main {
   margin-top: 100px;
@@ -58,7 +64,7 @@ main {
   margin-left: calc(15vw - 130px);
 }
 
-.titulo-descricao{
+.titulo-descricao {
   font-size: 20px;
   padding: 100px;
   text-align: center;
