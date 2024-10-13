26e6b2d5f1515fb452d707e1c96bdda6d920f5f7
isabellirosa <isabelli.ifc@gmail.com>
2024-10-12 22:34:45 -0300

    FEAT: Adicionando filtros na store #35

--- src/components/filtros/AsideFiltro.vue
+++ src/components/filtros/AsideFiltro.vue
@@ -1,275 +1,262 @@
 <script setup>
-import { ref, computed } from 'vue'
-const precoMin = ref(null)
-const precoMax = ref(null)
-const material = ref([])
-const potencia = ref([])
-const marca = ref([])
-
-const produtos = [
-    {
-        nome: 'Kit Energia Solar 1,14kWp 570W',
-        imageUrl: 'placa',
-        preco: 1000.00,
-        material: ['aço'],
-        potencia: 500,
-        marca: 'elgin',
-        parcelas: 'em até 10x de R$ 100,00',
-        qntdVendas: 1,
-        VerMais: {
-            'CARACTERÍSTICAS': 'Economia Imediata Certificado pelo INMETRO Resistente'
-        }
-    },
-    {
-        nome: 'Painel Solar 450W Monocristalino Half-Cell',
-        imageUrl: 'placa2',
-        preco: 1000.00,
-        parcelas: 'em até 10x de R$ 100,00',
-        material: ['ferro'],
-        potencia: 100,
-        marca: 'elgin',
-        qntdVendas: 2,
-        VerMais: {
-            'CARACTERÍSTICAS': 'Economia Imediata Certificado pelo INMETRO Resistente'
-        }
-    },
-    {
-        nome: 'Cabo Solar 4mm 30M Preto',
-        imageUrl: 'caboSolar',
-        preco: 1000.00,
-        parcelas: 'em até 10x de R$ 100,00',
-        material: ['aço'],
-        potencia: 400,
-        marca: 'weg',
-        qntdVendas: 3,
-        VerMais: {
-            'CARACTERÍSTICAS': 'Economia Imediata Certificado pelo INMETRO Resistente'
-        }
-    },
-    {
-        nome: 'Kit Energia Solar 1,14kWp 570W',
-        imageUrl: 'estruturaSolar',
-        preco: 2000.00,
-        parcelas: 'em até 10x de R$ 100,00',
-        material: ['ferro'],
-        potencia: 500,
-        marca: 'weg',
-        qntdVendas: 2,
-        VerMais: {
-            'CARACTERÍSTICAS': 'Economia Imediata Certificado pelo INMETRO Resistente'
-        }
-    },
-    {
-        nome: 'Kit Energia Solar 1,14kWp 570W',
-        imageUrl: 'placa3',
-        preco: 2000.00,
-        parcelas: 'em até 10x de R$ 100,00',
-        material: ['aço'],
-        potencia: 200,
-        marca: 'elgin',
-        qntdVendas: 1,
-        VerMais: {
-            'CARACTERÍSTICAS': 'Economia Imediata Certificado pelo INMETRO Resistente'
-        }
-    },
-    {
-        nome: 'Kit Energia Solar 1,14kWp 570W',
-        imageUrl: 'placa',
-        preco: 1000.00,
-        parcelas: 'em até 10x de R$ 100,00',
-        material: ['ferro'],
-        potencia: '900',
-        marca: 'weg',
-        qntdVendas: 3,
-        VerMais: {
-            'descricao': 'Caixa de Conexão IP 68 com 3 diodos de by-pass',
-        }
-    }
-]
-
-const filter = computed(() => {
-    function filterAndSort(produtos, filtredMateriais, filtredPotencia, filtredMarcas, minPrice, maxPrice, sortOrder) {
-
-        const newFiltredMateriais = new Set(filtredMateriais);
-        let arrayFiltrada = produtos
-
-        // Filtrando produtos com base em materiais
-        if (filtredMateriais.length != 0) {
-            arrayFiltrada = produtos.filter(item =>
-                item.material.some(element => newFiltredMateriais.has(element))
-            );
-        }
-
-        // Filtrando produtos com base em potência
-        if (filtredPotencia.length != 0) {
-            arrayFiltrada = arrayFiltrada.filter(item => filtredPotencia.includes(item.potencia));
-        }
-        // Filtrando produtos com base em marcas
-        if (filtredMarcas.length != 0) {
-            arrayFiltrada = arrayFiltrada.filter(item => filtredMarcas.includes(item.marca));
-        }
-        // Filtrando produtos com base no preço
-        if (minPrice != null && maxPrice != null) {
-            arrayFiltrada = arrayFiltrada.filter(item => item.preco >= minPrice && item.preco <= maxPrice);
-        }
-        // Ordenando produtos
-        if (sortOrder == 'menorPreco') {
-            arrayFiltrada.sort((a, b) => a.preco - b.preco);
-        }
-        else if (sortOrder == 'maiorPreco') {
-            arrayFiltrada.sort((a, b) => b.preco - a.preco);
-        }
-        else if (sortOrder == 'vendas') {
-
-            arrayFiltrada.sort((a, b) => b.qntdVendas - a.qntdVendas);
-        }
-        console.log(arrayFiltrada);
-        return arrayFiltrada
-    } 
-    return filterAndSort(produtos, material.value, potencia.value, marca.value, precoMin.value, precoMax.value, 'maiorPreco');
+import { ref } from "vue";
+import { useFiltroStore } from "@/stores/filtros";
+const FiltrosStore = useFiltroStore();
+const materialAberto = ref(true);
+const potenciaAberto = ref(true);
+const marcasAberto = ref(true);
+const precoAberto = ref(true);
+const precoMin = ref("");
+const precoMax = ref("");
+function clicked() {
+  FiltrosStore.precoMin = precoMin.value;
+  FiltrosStore.precoMax = precoMax.value;
 }
-)
-// Exemplo de uso
-
-
+function fecharPreco() {
+  FiltrosStore.precoMin = "";
+  FiltrosStore.precoMax = "";
+  precoAberto.value = false;
+}
+function fecharMaterial() {
+  FiltrosStore.material = [];
+  materialAberto.value = false;
+}
+function fecharPotencia() {
+  FiltrosStore.potencia = [];
+  potenciaAberto.value = false;
+}
+function fecharMarcas() {
+  FiltrosStore.marca = [];
+  marcasAberto.value = false;
+}
 </script>
+
 <template>
-    <div class="filtros">
-        <span class="subtitulo filtro-titulo">Filtros {{ filter }} {{ [precoMin, precoMax, material, potencia, marca] }}</span>
-        <div class="container">
-            <span class="subtitulo">Preço</span>
-            <span>-</span>
-        </div>
-        <div class="container">
-            <input class="input-min-max" placeholder="min" v-model="precoMin" />
-            <span>-</span>
-            <input class="input-min-max" placeholder="max" v-model="precoMax" />
-            <span>></span>
-        </div>
-        <div class="container">
-            <span class="subtitulo">Material</span>
-            <span>-</span>
-        </div>
-        <div class="opcoes">
-            <div class="opcao-input">
-                <input class="opcao" type="checkbox" id="ferro" name="ferro" value="ferro" v-model="material" />
-                <label class="opcao-input-label" for="ferro"> ferro </label>
-            </div>
-            <div class="opcao-input">
-                <input class="opcao" type="checkbox" id="Placas solares" name="Placas solares" value="Placas solares" />
-                <label class="opcao-input-label" for="Placas solares"> Placas solares </label>
-            </div>
-            <div class="opcao-input">
-                <input class="opcao" type="checkbox" id="Placas solares" name="Placas solares" value="Placas solares" />
-                <label class="opcao-input-label" for="Placas solares"> Placas solares </label>
-            </div>
-            <div class="opcao-input">
-                <input class="opcao" type="checkbox" id="Placas solares" name="Placas solares" value="Placas solares" />
-                <label class="opcao-input-label" for="Placas solares"> Placas solares </label>
-            </div>
-        </div>
-
-        <div>
-
-            <div class="container">
-                <span class="subtitulo">Potência</span>
-                <span>-</span>
-            </div>
-            <div class="opcoes">
-                <div class="opcao-input">
-                    <input class="opcao" type="checkbox" id="900" name="900" value="900" v-model="potencia" />
-                    <label class="opcao-input-label" for="900"> 900 </label>
-                </div>
-                <div class="opcao-input">
-                    <input class="opcao" type="checkbox" id="Placas solares" name="Placas solares"
-                        value="Placas solares" />
-                    <label class="opcao-input-label" for="Placas solares"> Placas solares </label>
-                </div>
-                <div class="opcao-input">
-                    <input class="opcao" type="checkbox" id="Placas solares" name="Placas solares"
-                        value="Placas solares" />
-                    <label class="opcao-input-label" for="Placas solares"> Placas solares </label>
-                </div>
-                <div class="opcao-input">
-                    <input class="opcao" type="checkbox" id="Placas solares" name="Placas solares"
-                        value="Placas solares" />
-                    <label class="opcao-input-label" for="Placas solares"> Placas solares </label>
-                </div>
-            </div>
-            <div class="container">
-                <span class="subtitulo">Marcas</span>
-                <span>+</span>
-            </div>
-            <div class="opcoes">
-                <div class="opcao-input">
-                    <input class="opcao" type="checkbox" id="weg" name="weg" value="weg" v-model="marca" />
-                    <label class="opcao-input-label" for="weg"> weg </label>
-                </div>
-                <div class="opcao-input">
-                    <input class="opcao" type="checkbox" id="Placas solares" name="Placas solares"
-                        value="Placas solares" />
-                    <label class="opcao-input-label" for="Placas solares"> Placas solares </label>
-                </div>
-                <div class="opcao-input">
-                    <input class="opcao" type="checkbox" id="Placas solares" name="Placas solares"
-                        value="Placas solares" />
-                    <label class="opcao-input-label" for="Placas solares"> Placas solares </label>
-                </div>
-                <div class="opcao-input">
-                    <input class="opcao" type="checkbox" id="Placas solares" name="Placas solares"
-                        value="Placas solares" />
-                    <label class="opcao-input-label" for="Placas solares"> Placas solares </label>
-                </div>
-            </div>
-        </div>
+  <div class="filtros">
+    <!-- <span
+      >{{ FiltrosStore.filtro }}
+      {{
+        [
+          FiltrosStore.precoMin,
+          FiltrosStore.precoMax,
+          FiltrosStore.material,
+          FiltrosStore.potencia,
+          FiltrosStore.marca,
+        ]
+      }}</span -->
+    <span class="subtitulo filtro-titulo">Filtros </span>
+    <div class="container">
+      <span class="subtitulo">Preço</span>
+      <button @click="fecharPreco()" v-if="precoAberto">-</button>
+      <button @click="precoAberto = !precoAberto" v-if="!precoAberto">+</button>
     </div>
+    <div class="container" v-if="precoAberto">
+      <input class="input-min-max" placeholder="min" v-model="precoMin" />
+      <span>-</span>
+      <input class="input-min-max" placeholder="max" v-model="precoMax" />
+      <button @click="clicked()">></button>
+    </div>
+    <div class="container">
+      <span class="subtitulo">Material</span>
+      <button @click="fecharMaterial()" v-if="materialAberto">-</button>
+      <button @click="materialAberto = !materialAberto" v-if="!materialAberto">+</button>
+    </div>
+    <div class="opcoes" v-if="materialAberto">
+      <div class="opcao-input">
+        <input
+          class="opcao"
+          type="checkbox"
+          id="ferro"
+          name="ferro"
+          value="ferro"
+          v-model="FiltrosStore.material"
+        />
+        <label class="opcao-input-label" for="ferro"> ferro </label>
+      </div>
+      <div class="opcao-input">
+        <input
+          class="opcao"
+          type="checkbox"
+          id="Placas solares"
+          name="Placas solares"
+          value="Placas solares"
+        />
+        <label class="opcao-input-label" for="Placas solares"> Placas solares </label>
+      </div>
+      <div class="opcao-input">
+        <input
+          class="opcao"
+          type="checkbox"
+          id="Placas solares"
+          name="Placas solares"
+          value="Placas solares"
+        />
+        <label class="opcao-input-label" for="Placas solares"> Placas solares </label>
+      </div>
+      <div class="opcao-input">
+        <input
+          class="opcao"
+          type="checkbox"
+          id="Placas solares"
+          name="Placas solares"
+          value="Placas solares"
+        />
+        <label class="opcao-input-label" for="Placas solares"> Placas solares </label>
+      </div>
+    </div>
+
+    <div>
+      <div class="container">
+        <span class="subtitulo">Potência</span>
+        <button @click="fecharPotencia()" v-if="potenciaAberto">-</button>
+        <button @click="potenciaAberto = !potenciaAberto" v-if="!potenciaAberto">
+          +
+        </button>
+      </div>
+      <div class="opcoes" v-if="potenciaAberto">
+        <div class="opcao-input">
+          <input
+            class="opcao"
+            type="checkbox"
+            id="900"
+            name="900"
+            value="900"
+            v-model="FiltrosStore.potencia"
+          />
+          <label class="opcao-input-label" for="900"> 900 </label>
+        </div>
+        <div class="opcao-input">
+          <input
+            class="opcao"
+            type="checkbox"
+            id="Placas solares"
+            name="Placas solares"
+            value="Placas solares"
+          />
+          <label class="opcao-input-label" for="Placas solares"> Placas solares </label>
+        </div>
+        <div class="opcao-input">
+          <input
+            class="opcao"
+            type="checkbox"
+            id="Placas solares"
+            name="Placas solares"
+            value="Placas solares"
+          />
+          <label class="opcao-input-label" for="Placas solares"> Placas solares </label>
+        </div>
+        <div class="opcao-input">
+          <input
+            class="opcao"
+            type="checkbox"
+            id="Placas solares"
+            name="Placas solares"
+            value="Placas solares"
+          />
+          <label class="opcao-input-label" for="Placas solares"> Placas solares </label>
+        </div>
+      </div>
+      <div class="container">
+        <span class="subtitulo">Marcas</span>
+        <button @click="fecharMarcas()" v-if="marcasAberto">-</button>
+        <button @click="marcasAberto = !marcasAberto" v-if="!marcasAberto">+</button>
+      </div>
+      <div class="opcoes" v-if="marcasAberto">
+        <div class="opcao-input">
+          <input
+            class="opcao"
+            type="checkbox"
+            id="weg"
+            name="weg"
+            value="weg"
+            v-model="FiltrosStore.marca"
+          />
+          <label class="opcao-input-label" for="weg"> weg </label>
+        </div>
+        <div class="opcao-input">
+          <input
+            class="opcao"
+            type="checkbox"
+            id="Placas solares"
+            name="Placas solares"
+            value="Placas solares"
+          />
+          <label class="opcao-input-label" for="Placas solares"> Placas solares </label>
+        </div>
+        <div class="opcao-input">
+          <input
+            class="opcao"
+            type="checkbox"
+            id="Placas solares"
+            name="Placas solares"
+            value="Placas solares"
+          />
+          <label class="opcao-input-label" for="Placas solares"> Placas solares </label>
+        </div>
+        <div class="opcao-input">
+          <input
+            class="opcao"
+            type="checkbox"
+            id="Placas solares"
+            name="Placas solares"
+            value="Placas solares"
+          />
+          <label class="opcao-input-label" for="Placas solares"> Placas solares </label>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <style scoped>
+button {
+  background-color: transparent;
+  border: none;
+}
 option {
-    background-color: transparent;
-    border-radius: 10px;
+  background-color: transparent;
+  border-radius: 10px;
 }
 
-.opcao-input input[type='checkbox']:checked,
-input[type='checkbox']:checked {
-    accent-color: #2F3F68;
+.opcao-input input[type="checkbox"]:checked,
+input[type="checkbox"]:checked {
+  accent-color: #2f3f68;
 }
 
 .opcao {
-    margin: 0px 15px;
+  margin: 0px 15px;
 }
 
 .container {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 10px 0px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 0px;
 }
 
 .input-min-max {
-    width: 40%;
-    border-radius: 10px;
-    background-color: transparent;
-    border: 1px solid #D9D9D9;
-    color: #bebdbd;
-    text-align: center;
+  width: 40%;
+  border-radius: 10px;
+  background-color: transparent;
+  border: 1px solid #d9d9d9;
+  color: #bebdbd;
+  text-align: center;
 }
 
 .filtros {
-    display: flex;
-    flex-direction: column;
-    width: 25vw;
-    margin-top: 20px;
-    padding-left: 7vw;
+  display: flex;
+  flex-direction: column;
+  width: 25vw;
+  margin-top: 20px;
+  padding-left: 7vw;
 }
 
 .filtro-titulo {
-    border-bottom: 1px solid #D9D9D9;
-    margin-bottom: 5px;
+  border-bottom: 1px solid #d9d9d9;
+  margin-bottom: 5px;
 }
 
 .subtitulo {
-    font-weight: 600;
-    padding: 10px 0px;
+  font-weight: 600;
+  padding: 10px 0px;
 }
 </style>
--- src/stores/filtros.js
+++ src/stores/filtros.js
@@ -1,145 +1,149 @@
-//IMPORTAR EM OUTRO LOCAL PARA FUNCIONAR
+import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
-export const useFilterStore = defineStore('filter', () => {
-  //   const filtredArray = ['livro']
-  //   const publication = { category: 'livro' }
-  //   const num=3
-  //   const filtredCategories = ['livro', 'artigo']
-  //   const filtredDates = [1,2,3,4,5,6,7]
-  //   function filterByCategories() {
-  //     console.log(filtredCategories.include(publication.category))
-  //     return filtredCategories.include(publication.category)
-  //   }
-  //   function filterByDate(publication, minDate, maxDate) {
-  //     return publication.date >= minDate && publication.date <= maxDate
-  //   }
-  //   function filtredPublications() {
-  //     filtredArray = usePublicationStore.entirePublications.filter((publication) => {
-  //       filterByCategories(publication, filtredCategories)
-  //       filterByDate(publication, minDate, maxDate)
-  //     })
-  //   }
-})
-
+export const useFiltroStore = defineStore('filtro', () => {
+  const precoMin = ref('')
+  const precoMax = ref('')
+  const material = ref([])
+  const potencia = ref([])
+  const marca = ref([])
 
   const produtos = [
     {
       nome: 'Kit Energia Solar 1,14kWp 570W',
       imageUrl: 'placa',
-      preco: 1000.00,
+      preco: 1000.0,
       material: ['aço'],
       potencia: 500,
       marca: 'elgin',
       parcelas: 'em até 10x de R$ 100,00',
       qntdVendas: 1,
       VerMais: {
-        'CARACTERÍSTICAS': 'Economia Imediata Certificado pelo INMETRO Resistente'
+        CARACTERÍSTICAS: 'Economia Imediata Certificado pelo INMETRO Resistente'
       }
     },
     {
       nome: 'Painel Solar 450W Monocristalino Half-Cell',
       imageUrl: 'placa2',
-      preco:  1000.00,
+      preco: 1000.0,
       parcelas: 'em até 10x de R$ 100,00',
       material: ['ferro'],
       potencia: 100,
       marca: 'elgin',
       qntdVendas: 2,
       VerMais: {
-        'CARACTERÍSTICAS': 'Economia Imediata Certificado pelo INMETRO Resistente'
+        CARACTERÍSTICAS: 'Economia Imediata Certificado pelo INMETRO Resistente'
       }
     },
     {
       nome: 'Cabo Solar 4mm 30M Preto',
       imageUrl: 'caboSolar',
-      preco:  1000.00,
+      preco: 1000.0,
       parcelas: 'em até 10x de R$ 100,00',
       material: ['aço'],
       potencia: 400,
       marca: 'weg',
       qntdVendas: 3,
       VerMais: {
-        'CARACTERÍSTICAS': 'Economia Imediata Certificado pelo INMETRO Resistente'
+        CARACTERÍSTICAS: 'Economia Imediata Certificado pelo INMETRO Resistente'
       }
     },
     {
       nome: 'Kit Energia Solar 1,14kWp 570W',
       imageUrl: 'estruturaSolar',
-      preco: 2000.00,
+      preco: 2000.0,
       parcelas: 'em até 10x de R$ 100,00',
       material: ['ferro'],
       potencia: 500,
       marca: 'weg',
       qntdVendas: 2,
       VerMais: {
-        'CARACTERÍSTICAS': 'Economia Imediata Certificado pelo INMETRO Resistente'
+        CARACTERÍSTICAS: 'Economia Imediata Certificado pelo INMETRO Resistente'
       }
     },
     {
       nome: 'Kit Energia Solar 1,14kWp 570W',
       imageUrl: 'placa3',
-      preco:  2000.00,
+      preco: 2000.0,
       parcelas: 'em até 10x de R$ 100,00',
       material: ['aço'],
       potencia: 200,
       marca: 'elgin',
       qntdVendas: 1,
       VerMais: {
-        'CARACTERÍSTICAS': 'Economia Imediata Certificado pelo INMETRO Resistente'
+        CARACTERÍSTICAS: 'Economia Imediata Certificado pelo INMETRO Resistente'
       }
     },
     {
       nome: 'Kit Energia Solar 1,14kWp 570W',
       imageUrl: 'placa',
-      preco:  1000.00,
+      preco: 1000.0,
       parcelas: 'em até 10x de R$ 100,00',
       material: ['ferro'],
-      potencia: 900,
+      potencia: '900',
       marca: 'weg',
       qntdVendas: 3,
       VerMais: {
-        'descricao': 'Caixa de Conexão IP 68 com 3 diodos de by-pass',
+        descricao: 'Caixa de Conexão IP 68 com 3 diodos de by-pass'
       }
     }
   ]
 
-function filterAndSort(produtos, filtredMateriais, filtredPotencia, filtredMarcas, minPrice, maxPrice, sortOrder) {
-
-  const newFiltredMateriais = new Set(filtredMateriais);
-  let arrayFiltrada = produtos
-
-  // Filtrando produtos com base em materiais
-  if (filtredMateriais != null) {
-  arrayFiltrada = produtos.filter(item =>
-    item.material.some(element => newFiltredMateriais.has(element))
-  );}
-
-  // Filtrando produtos com base em potência
-  if (filtredPotencia != null) {
-  arrayFiltrada = arrayFiltrada.filter(item => filtredPotencia.includes(item.potencia));
-  }
-  // Filtrando produtos com base em marcas
-  if (filtredPotencia != null) {
-  arrayFiltrada = arrayFiltrada.filter(item => filtredMarcas.includes(item.marca));
-  }
-  // Filtrando produtos com base no preço
-  if (filtredPotencia != null) {
-  arrayFiltrada = arrayFiltrada.filter(item => item.preco >= minPrice && item.preco <= maxPrice);
-  }
-  // Ordenando produtos
-  if (sortOrder == 'menorPreco') { 
-    arrayFiltrada.sort((a, b) => a.preco - b.preco);
-  } 
-  else if (sortOrder == 'maiorPreco') {
-    arrayFiltrada.sort((a, b) => b.preco - a.preco);
-  } 
-  else if (sortOrder == 'vendas') {
-
-    arrayFiltrada.sort((a, b) => b.qntdVendas - a.qntdVendas);
-  }
-  console.log(arrayFiltrada);
-}
-// Exemplo de uso
-filterAndSort(produtos, null , null , null, null, null, 'maiorPreco');
-
+  const filtro = computed(() => {
+    function filterAndSort(
+      produtos,
+      filtredMateriais,
+      filtredPotencia,
+      filtredMarcas,
+      minPrice,
+      maxPrice,
+      sortOrder
+    ) {
+      const newFiltredMateriais = new Set(filtredMateriais)
+      let arrayFiltrada = produtos
+      // Filtrando produtos com base em materiais
+      if (filtredMateriais.length != 0) {
+        arrayFiltrada = produtos.filter((item) =>
+          item.material.some((element) => newFiltredMateriais.has(element))
+        )
+      }
+      // Filtrando produtos com base em potência
+      if (filtredPotencia.length != 0) {
+        arrayFiltrada = arrayFiltrada.filter((item) => filtredPotencia.includes(item.potencia))
+      }
+      // Filtrando produtos com base em marcas
+      if (filtredMarcas.length != 0) {
+        arrayFiltrada = arrayFiltrada.filter((item) => filtredMarcas.includes(item.marca))
+      }
+      // Filtrando produtos com base no preço
+      if (minPrice != '' && maxPrice != '') {
+        arrayFiltrada = arrayFiltrada.filter(
+          (item) => item.preco >= minPrice && item.preco <= maxPrice
+        )
+      } else if (minPrice != '' ) {
+        arrayFiltrada = arrayFiltrada.filter((item) => item.preco >= minPrice)
+      } else if (maxPrice != '' ) {
+        arrayFiltrada = arrayFiltrada.filter((item) => item.preco <= maxPrice)
+      }
+      // Ordenando produtos
+      if (sortOrder == 'menorPreco') {
+        arrayFiltrada.sort((a, b) => a.preco - b.preco)
+      } else if (sortOrder == 'maiorPreco') {
+        arrayFiltrada.sort((a, b) => b.preco - a.preco)
+      } else if (sortOrder == 'vendas') {
+        arrayFiltrada.sort((a, b) => b.qntdVendas - a.qntdVendas)
+      }
+      console.log(arrayFiltrada)
+      return arrayFiltrada
+    }
+    return filterAndSort(
+      produtos,
+      material.value,
+      potencia.value,
+      marca.value,
+      precoMin.value,
+      precoMax.value,
+    )
+  })
+  return { precoMin, precoMax, material, potencia, marca, produtos, filtro }
+})
