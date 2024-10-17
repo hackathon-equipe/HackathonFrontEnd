1cb6697a25ee2d5ac714ee6c715c72f206679b19
vinyzada <vinyescolas@gmail.com>
2024-10-17 16:46:53 -0300

    feat: carrinho agora funcional, função de add, remover, cards, calculo de preço e icone do carrinho atualiza conforme a quantidade #13

---
+++ src/components/icons/xCloseIcon.vue
@@ -0,0 +1,5 @@
+<template>
+    <svg xmlns="http://www.w3.org/2000/svg" width="39" height="39" viewBox="0 0 39 39" fill="none">
+        <path d="M29.25 9.75L9.75 29.25M9.75 9.75L29.25 29.25" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+</template>
