12cca48c442e6e44abea4485f4f1c035bf349066
isabellirosa <isabelli.ifc@gmail.com>
2024-11-21 21:38:25 -0300

    FEAT: Iniciando integração #67

--- src/components/carrinho/componentes/resumoCarrinho.vue
+++ src/components/carrinho/componentes/resumoCarrinho.vue
@@ -1,7 +1,57 @@
 <script setup>
   import inputAplicarComp from './inputAplicarComp.vue';
   import { useCartStore } from '@/stores/carrinhoStore';
+  import { ref, reactive } from 'vue';
+ import axios from 'axios'; // Importando Axios
   const cart = useCartStore()
+
+ const orderData = reactive({ "title": "Compra na loja oorun", "quantity": cart.itensInCart, "price": cart.valorNoCarrinho});
+ 
+ // MercadoPago initialization
+ const mp = new MercadoPago('APP_USR-b2ad37f2-01f8-4ed9-b5be-7ddb974c6eb0', { locale: 'pt-BR' });
+ 
+ // Reactive state to hold preference ID
+ const preferenceId = ref(null);
+ 
+ // Função para criar o botão de checkout após obter o ID da preferência
+ const createCheckoutButton = (preferenceId) => {
+   const bricksBuilder = mp.bricks();
+ 
+   const renderComponent = async () => {
+     // Remover qualquer botão anterior, se existir (gerenciado pelo Vue agora)
+     // Criar o botão de checkout do Mercado Pago no 'wallet_container'
+     await bricksBuilder.create('wallet', 'wallet_container', {
+       initialization: {
+         preferenceId: preferenceId,
+       },
+     });
+   };
+ 
+   renderComponent();
+ };
+ 
+ // Função para manipular o clique e buscar os dados de preferência
+ const handleCheckoutClick = async () => {
+   try {
+     // Enviar uma requisição para o backend para criar a preferência
+     const response = await axios.post('https://backend-api-mercadopago.onrender.com/create_preference', orderData, {
+       headers: {
+         'Content-Type': 'application/json',
+       },
+     });
+ 
+     // Obter o ID da preferência da resposta
+     const preference = response.data;
+ 
+     // Armazenar o ID da preferência e criar o botão de checkout
+     preferenceId.value = preference.id;
+     createCheckoutButton(preference.id);
+   } catch (error) {
+     // Tratar erros da requisição
+     alert('Erro: Não foi possível criar a preferência de pagamento.');
+     console.error(error);
+   }
+ };
 </script>
 
 <template>
@@ -20,7 +70,8 @@
       </inputAplicarComp>
       <inputAplicarComp titulo="Calcular Frete" tituloBotao="Inserir CEP" inputPlaceholder="Insira seu CEP" />
     </div>
-    <router-link to="/pagamento" class="finalizar-compra">Finalizar compra</router-link>
+    <button @click="handleCheckoutClick" class="finalizar-compra">Finalizar compra</button>
+    <div id="wallet_container"></div>
   </div>
 </template>
 <style scoped>
