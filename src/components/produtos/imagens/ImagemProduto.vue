868702cb165010ebeef86d08a8d0fb0ffadb2970
julia <juliafu713@gmail.com>
2024-11-26 15:07:29 -0300

    fix: foto produto arrumada #75

--- src/components/produtos/imagens/ImagemProduto.vue
+++ src/components/produtos/imagens/ImagemProduto.vue
@@ -1,13 +1,55 @@
 <script setup>
+import { ref } from 'vue';
+
 defineProps({
-  imagem: String
-})
+  imagem: Array
+});
+
+// Referência para o elemento
+const imgRef = ref(null);
+
+// Estado para rastrear o hover
+const isHovering = ref(false);
+
+const handleMouseMove = (event) => {
+  const img = imgRef.value;
+
+  // Calcula as posições relativas do mouse
+  const rect = img.getBoundingClientRect();
+  const x = ((event.clientX - rect.left) / rect.width) * 100;
+  const y = ((event.clientY - rect.top) / rect.height) * 100;
+
+  // Atualiza as variáveis CSS
+  img.style.setProperty('--x', `${x}%`);
+  img.style.setProperty('--y', `${y}%`);
+};
+
+const handleMouseEnter = () => {
+  isHovering.value = true;
+};
+
+const handleMouseLeave = () => {
+  isHovering.value = false;
+};
 </script>
+
 <template>
-      <div class="img">
-        <img class="oi" :src="imagem" />
-      </div>
+  <div 
+    class="img" 
+    @mousemove="handleMouseMove" 
+    @mouseenter="handleMouseEnter" 
+    @mouseleave="handleMouseLeave"
+  >
+    <img 
+      ref="imgRef" 
+      class="zoom" 
+      :class="{ 'zoom-active': isHovering }" 
+      :src="imagem.file" 
+      alt="Imagem" 
+    />
+  </div>
 </template>
+
 <style scoped>
 .img {
   border: 1px solid #d9d9d9;
@@ -18,5 +60,21 @@ defineProps({
   margin-right: 50px;
   padding: 40px;
   width: 35vw;
+  height: 50vh;
+  overflow: hidden;
+  position: relative;
+  cursor: pointer;
+}
+
+.img img {
+  max-width: 100%;
+  max-height: 100%;
+  object-fit: contain;
+  transition: transform 0.3s ease, transform-origin 0.3s ease; /* Suaviza todas as transições */
+}
+
+.img img.zoom-active {
+  transform: scale(1.8);
+  transform-origin: var(--x) var(--y);
 }
 </style>
--- src/components/produtos/imagens/ImagensPqn.vue
+++ src/components/produtos/imagens/ImagensPqn.vue
@@ -1,35 +1,75 @@
 <script setup>
-defineProps({
-  imagem: String
+import { ref } from 'vue';
+const props = defineProps({
+  images: Array
 })
+
+const emit = defineEmits(['selecionarImage'])
+function selecionarImagem(imagem) {
+  emit('selecionarImage', imagem) // Emitindo o evento com a imagem selecionada
+  console.log(imagem)
+  selectedImage.value = imagem.id; 
+}
+const vazio = props.images.length == 1 ? 3 : props.images.length == 2 ? 2 :props.images.length == 3 ? 1:0
+const selectedImage = ref(props.images.length > 0 ? props.images[0].id : null);
+
+// Emite o evento com a primeira imagem ao carregar o componente
+if (props.images.length > 0) {
+  emit('selecionarImage', props.images[0]);
+}
+
 </script>
 <template>
-    <div class="imgs">
-        <div class="img-pqn"><img :src="imagem" /></div>
-        <div class="img-pqn"><img :src="imagem" /></div>
-        <div class="img-pqn"><img :src="imagem" /></div>
-        <div class="img-pqn"><img :src="imagem" /></div>
-      </div>
+  <div class="imgs">
+    <div v-for="image in props.images" :key="image.id" :class="{ selectTrue: selectedImage === image.id }">
+      <div class="img-pqn">
+      <img :src="image.file" @click="selecionarImagem(image)" /></div>
+    </div>
+      <div v-for="n in vazio" :key="n" class="img-pqn " id="disabilitado"></div>
+  </div>
+
+  <!-- <div class="img-pqn" v-if="imagem[0]"><img :src="imagem[0].file" /></div>
+        <div class="img-pqn" v-if="imagem[1]"><img :src="imagem[1].file" /></div>
+        <div class="img-pqn disabilitado" v-else></div>
+        <div class="img-pqn" v-if="imagem[2]"><img :src="imagem[2].file" /></div>
+        <div class="img-pqn disabilitado" v-else></div>
+        <div class="img-pqn" v-if="imagem[3]"><img :src="imagem[3].file" /></div>
+        <div class="img-pqn disabilitado" v-else></div>
+      </div> -->
 </template>
 <style scoped>
 .imgs {
-    display: flex;
-    justify-content: space-between;
-    width: 35vw;
-    margin-left: calc(15vw - 30px);
-    margin-top: 1vw;
-  }
-  .img-pqn {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 8vw;
-    border: 1px solid #d9d9d9;
-    border-radius: 20px;
-  }
-  
-  .img-pqn img {
-    width: 100%;;
-    padding: 15px;
-  }
+  display: flex;
+  justify-content: space-between;
+  width: 35vw;
+  height: 15vh;
+  margin-left: calc(15vw - 30px);
+  margin-top: 1vw;
+}
+.imgs .img-pqn {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 8vw;
+  border: 1px solid #d9d9d9;
+  border-radius: 20px;
+  height: 100%;
+  cursor: pointer;
+
+}
+
+#disabilitado {
+  background-color: #dfdfdfa1;
+  cursor: not-allowed;
+}
+
+.img-pqn img {
+  width: 100%;
+  padding: 15px;
+}
+
+.selectTrue{
+  border: 2px solid #d9d9d9;
+  border-radius: 20px;
+}
 </style>
