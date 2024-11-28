1cdf701e94aa4cc334512b3858c39bdb0df616f0
vinyzada <vinyescolas@gmail.com>
2024-11-27 21:02:38 -0300

    feat: criado composable para verificar tamanho de tela, definido tamanho mobile para 768px, pagina home agr mobile #63

--- src/components/homeComponents/contactComp.vue
+++ src/components/homeComponents/contactComp.vue
@@ -82,4 +82,18 @@ import { useScreenSize } from '@/composables/useScreenSize';
         justify-content: center;
     }
 }
+@media (max-width: 768px) {
+    .container{
+        flex-direction: column;
+        margin: 10px 30px;
+    }
+    .content{
+        text-align: center;
+        align-items: center;
+    }
+    .images{
+        display: flex;
+        justify-content: center;
+    }
+}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -32,7 +32,6 @@ useAuth()
 onMounted(() => {
   getUserData()
 })
-
 </script>
 
 <template>
@@ -80,29 +79,35 @@ onMounted(() => {
 .fade-leave-to {
   opacity: 0;
 }
-@media (max-width: 700px) {
-  .home{
+
+@media (max-width: 768px) {
+  .home {
     flex-direction: column-reverse;
     align-items: center;
     padding: 40px 40px 40px 40px;
   }
-  .home .info-side .home-tittle h1{
+
+  .home .info-side .home-tittle h1 {
     font-size: 32px;
     line-height: 40px;
     text-align: center;
   }
-  .home .info-side .home-icons{
+
+  .home .info-side .home-icons {
     display: none;
   }
-  .home .info-side .home-buttons{
+
+  .home .info-side .home-buttons {
     display: flex;
     gap: 0px;
     flex-direction: column-reverse;
   }
-  .home .info-side .home-buttons .button{
+
+  .home .info-side .home-buttons .button {
     justify-content: center;
   }
-  .home .home-buttons .button:first-child{
+
+  .home .home-buttons .button:first-child {
     background-color: transparent;
     color: black;
   }
